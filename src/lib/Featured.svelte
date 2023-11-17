b1c7f9c0f8707f74c237c4d81aaa54a8135a51cc
Marshall Bowles <marshall.bowles@gmail.com>
2023-11-17 14:44:34 -0800

    add embla carousel for featured items on mobile

--- src/lib/Featured.svelte
+++ src/lib/Featured.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
+
 	export let featuredItems: any = [
 		{ url: '/test', image: '/avatar.png', name: 'placeholder' },
 		{ url: '/test', image: '/avatar.png', name: 'placeholder' },
 		{ url: '/test', image: '/avatar.png', name: 'placeholder' }
 	];
+
+  let emblaOptions: any = { loop: true };
+  let emblaApi: any;
+
+  function onInit(event: any) {
+    emblaApi = event.detail;
+  }
+
+  function slideLeft() {
+    emblaApi.scrollPrev();
+  }
+
+  function slideRight() {
+    emblaApi.scrollNext();
+  }
 </script>
 
 <h3 class="h3 mb-8">Featured</h3>
@@ -18,12 +35,35 @@
 </div>
 
 <div class="flex w-1/2 flex-row items-center justify-evenly md:hidden">
-  TODO: Gallery
-	<!-- {#each featuredItems as item, i}
-		<div class="flex flex-col items-center">
-			<img class="w-1/2" src={item.image} alt={item.name} />
-			{item.url}
-			{i}
+  <button class="btn-icon btn-sm mr-4" on:click={slideLeft}>
+    <span class="fa-solid fa-chevron-left"></span>
+  </button>
+
+	<div class="embla" use:emblaCarouselSvelte="{{ emblaOptions }}" on:emblaInit="{onInit}">
+		<div class="embla__container">
+      {#each featuredItems as item}
+			  <div class="embla__slide">
+          <img src={item.image} alt={item.name} />
+        </div>
+      {/each}
 		</div>
-	{/each} -->
+	</div>
+
+  <button class="btn-icon btn-sm ml-4" on:click={slideRight}>
+    <span class="fa-solid fa-chevron-right"></span>
+  </button>
 </div>
+
+<style>
+  .embla {
+    overflow: hidden;
+  }
+  .embla__container {
+    display: flex;
+  }
+  .embla__slide {
+    flex: 0 0 100%;
+    min-width: 0;
+  }
+</style>
+
