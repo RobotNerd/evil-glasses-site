efbfce9329f9233a3e1696e4375c84fa9494e873
Marshall <marshall.bowles@gmail.com>
2023-11-21 13:27:23 -0800

    fix pdf download link

--- src/routes/short-stories/[date]/[edition]/+page.svelte
+++ src/routes/short-stories/[date]/[edition]/+page.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
-  import DetailHeader from "$lib/DetailHeader.svelte";
-  import { page } from '$app/stores';
-  import { Stories } from "$lib/data/short-story-meta";
+	import DetailHeader from '$lib/DetailHeader.svelte';
+	import { page } from '$app/stores';
+	import { Stories } from '$lib/data/short-story-meta';
 
-  const edition: number = parseInt($page.params.edition);
-  const image = `/short-stories/${$page.params.date}-edition-${$page.params.edition}.png`;
-  const imageAlt = `Evil Glasses short stories edition ${edition}`;
+	const edition: number = parseInt($page.params.edition);
+	const image = `/short-stories/${$page.params.date}-edition-${$page.params.edition}.png`;
+	const imageAlt = `Evil Glasses short stories edition ${edition}`;
+	const pdfLink = `/short-stories/${$page.params.date}-edition-${$page.params.edition}.pdf`;
+
+	console.log('link: ', pdfLink);
 </script>
 
-<DetailHeader
-  title="Evil Glasses Short Stories"
-  subtitle="Edition {edition}"
-  image="{image}"
-  imageAlt="{imageAlt}" />
+<DetailHeader title="Evil Glasses Short Stories" subtitle="Edition {edition}" {image} {imageAlt} />
 
 <h3 class="h3 mb-4">Read Online</h3>
 
 <div>
-  {#each Stories[edition].stories as story}
-    <div>
-      <a class="underline" href="/short-stories/read/{$page.params.date}/{story.title}">{story.title}</a>
-      by {story.author}
-    </div>
-  {/each}
+	{#each Stories[edition].stories as story}
+		<div>
+			<a class="underline" href="/short-stories/read/{$page.params.date}/{story.title}"
+				>{story.title}</a
+			>
+			by {story.author}
+		</div>
+	{/each}
 </div>
 
 <h3 class="h3 my-4">Or</h3>
 
-<button type="button" class="btn variant-filled-primary">  <!-- TODO pdf download -->
-  <span class="fa-regular fa-file-pdf" />
-  <span>Download PDF</span>
-</button>
+<a class="variant-filled-secondary rounded-lg p-4" href={pdfLink}>
+	<span class="fa-regular fa-file-pdf" />
+	Download PDF
+</a>
