52ad8b3d3561780286126478df64d654620d16f0
Marshall <marshall.bowles@gmail.com>
2023-11-21 19:12:56 -0800

    refactor privacy policy

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,3 +1,11 @@
 <h1 class="h1">Legal</h1>
 
-TODO CONTENT
+<div class="mt-8 flex w-2/3 sm:w-1/2 flex-col content-start">
+	<h3 class="h3 mb-4">Privacy Policies</h3>
+	<a
+		class="card variant-ghost card-hover mb-4 w:3/4 sm:w-1/2 rounded-lg p-4 underline text-center"
+		href="/legal/inflation-buster/"
+	>
+		Inflation Buster
+	</a>
+</div>
