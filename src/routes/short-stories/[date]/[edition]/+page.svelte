59364ccea2a0328e9c43e5ea4a6cdb458871cfb8
Marshall Bowles <marshall.bowles@gmail.com>
2023-11-24 20:23:17 -0800

    fix short story title to work with prerendering

--- src/routes/short-stories/[date]/[edition]/+page.svelte
+++ src/routes/short-stories/[date]/[edition]/+page.svelte
@@ -7,6 +7,10 @@
 	const image = `/short-stories/${$page.params.date}-edition-${$page.params.edition}.png`;
 	const imageAlt = `Evil Glasses short stories edition ${edition}`;
 	const pdfLink = `/short-stories/${$page.params.date}-edition-${$page.params.edition}.pdf`;
+
+	function sanitizeTitle(title: string): string {
+		return title.replace("'", "");
+	}
 </script>
 
 <DetailHeader title="Evil Glasses Short Stories" subtitle="Edition {edition}" {image} {imageAlt} />
@@ -17,7 +21,7 @@
 	{#each Stories[edition].stories as story}
 		<a
 			class="card variant-ghost card-hover mb-8 flex flex-col rounded-lg p-4"
-			href="/short-stories/read/{$page.params.date}/{story.title}"
+			href="/short-stories/read/{$page.params.date}/{sanitizeTitle(story.title)}"
 		>
 			<h3 class="h3">{story.title}</h3>
 			by {story.author}
