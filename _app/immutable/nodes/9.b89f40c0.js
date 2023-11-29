5fb6d55d9dd630460d43cf46db649579478554c6
Marshall <marshall.bowles@gmail.com>
2023-11-29 13:20:48 -0800

    Updates

---
+++ _app/immutable/nodes/9.b89f40c0.js
@@ -0,0 +1,40 @@
+import{s as y,f as p,a as m,g as d,Y as h,c as f,j as v,i as o,H as r,d as n}from"../chunks/scheduler.2c32cff6.js";import{S as w,i as g}from"../chunks/index.4fb3896e.js";function b(u){let e,l="Privacy Policy for Inflation Buster",i,s,c=`<h2 class="h2 svelte-dp9awi">Privacy Policy</h2> <p class="svelte-dp9awi">Evil Glasses built the Inflation Buster app as an Ad Supported app. This SERVICE is provided by
+		Evil Glasses at no cost and is intended for use as is.</p> <p class="svelte-dp9awi">This page is used to inform visitors regarding my policies with the collection, use, and
+		disclosure of Personal Information if anyone decided to use my Service.</p> <p class="svelte-dp9awi">If you choose to use my Service, then you agree to the collection and use of information in
+		relation to this policy. The Personal Information that I collect is used for providing and
+		improving the Service. I will not use or share your information with anyone except as described
+		in this Privacy Policy.</p> <p class="svelte-dp9awi">The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions,
+		which are accessible at Inflation Buster unless otherwise defined in this Privacy Policy.</p> <h2 class="h2 svelte-dp9awi">Information Collection and Use</h2> <p class="svelte-dp9awi">For a better experience, while using our Service, I may require you to provide us with certain
+		personally identifiable information, including but not limited to IP address, User product
+		interactions, Diagnostic information, Device and Account identifiers. The information that I
+		request will be retained on your device and is not collected by me in any way.</p> <div><p class="svelte-dp9awi">The app does use third-party services that may collect information used to identify you.</p> <p class="svelte-dp9awi">Links to the privacy policies of third-party service providers used by the app</p> <ul class="svelte-dp9awi"><li class="svelte-dp9awi"><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer" class="svelte-dp9awi">AdMob</a></li> <li class="svelte-dp9awi"><a href="https://godotengine.org/privacy-policy" target="_blank" rel="noopener noreferrer" class="svelte-dp9awi">Godot</a></li></ul></div> <h2 class="h2 svelte-dp9awi">Log Data</h2> <p class="svelte-dp9awi">I want to inform you that whenever you use my Service, in a case of an error in the app I
+		collect data and information (through third-party products) on your phone called Log Data. This
+		Log Data may include information such as your device Internet Protocol (“IP”) address, device
+		name, operating system version, the configuration of the app when utilizing my Service, the time
+		and date of your use of the Service, and other statistics.</p> <h2 class="h2 svelte-dp9awi">Cookies</h2> <p class="svelte-dp9awi">Cookies are files with a small amount of data that are commonly used as anonymous unique
+		identifiers. These are sent to your browser from the websites that you visit and are stored on
+		your device&#39;s internal memory.</p> <p class="svelte-dp9awi">This Service does not use these “cookies” explicitly. However, the app may use third-party code
+		and libraries that use “cookies” to collect information and improve their services. You have the
+		option to either accept or refuse these cookies and know when a cookie is being sent to your
+		device. If you choose to refuse our cookies, you may not be able to use some portions of this
+		Service.</p> <h2 class="h2 svelte-dp9awi">Service Providers</h2> <p class="svelte-dp9awi">I may employ third-party companies and individuals due to the following reasons:</p> <ul class="svelte-dp9awi"><li class="svelte-dp9awi">To facilitate our Service;</li> <li class="svelte-dp9awi">To provide the Service on our behalf;</li> <li class="svelte-dp9awi">To perform Service-related services; or</li> <li class="svelte-dp9awi">To assist us in analyzing how our Service is used.</li></ul> <p class="svelte-dp9awi">I want to inform users of this Service that these third parties have access to their Personal
+		Information. The reason is to perform the tasks assigned to them on our behalf. However, they
+		are obligated not to disclose or use the information for any other purpose.</p> <h2 class="h2 svelte-dp9awi">Security</h2> <p class="svelte-dp9awi">I value your trust in providing us your Personal Information, thus we are striving to use
+		commercially acceptable means of protecting it. But remember that no method of transmission over
+		the internet, or method of electronic storage is 100% secure and reliable, and I cannot
+		guarantee its absolute security.</p> <h2 class="h2 svelte-dp9awi">Links to Other Sites</h2> <p class="svelte-dp9awi">This Service may contain links to other sites. If you click on a third-party link, you will be
+		directed to that site. Note that these external sites are not operated by me. Therefore, I
+		strongly advise you to review the Privacy Policy of these websites. I have no control over and
+		assume no responsibility for the content, privacy policies, or practices of any third-party
+		sites or services.</p> <h2 class="h2 svelte-dp9awi">Children&#39;s Privacy</h2> <div><p class="svelte-dp9awi">These Services do not address anyone under the age of 13. I do not knowingly collect
+			personally identifiable information from children under 13 years of age. In the case I
+			discover that a child under 13 has provided me with personal information, I immediately delete
+			this from our servers. If you are a parent or guardian and you are aware that your child has
+			provided us with personal information, please contact me so that I will be able to do the
+			necessary actions.</p></div> <h2 class="h2 svelte-dp9awi">Changes to This Privacy Policy</h2> <p class="svelte-dp9awi">I may update our Privacy Policy from time to time. Thus, you are advised to review this page
+		periodically for any changes. I will notify you of any changes by posting the new Privacy Policy
+		on this page.</p> <p class="svelte-dp9awi">This policy is effective as of November 29, 2023.</p> <h2 class="h2 svelte-dp9awi">Contact Us</h2> <p class="svelte-dp9awi">If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me
+		at contact@evilglasses.com.</p> <p class="svelte-dp9awi">This privacy policy page was created at
+		<a href="https://privacypolicytemplate.net" target="_blank" rel="noopener noreferrer" class="svelte-dp9awi">privacypolicytemplate.net
+		</a>and modified/generated by
+		<a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer" class="svelte-dp9awi">App Privacy Policy Generator</a></p>`;return{c(){e=p("h2"),e.textContent=l,i=m(),s=p("div"),s.innerHTML=c,this.h()},l(t){e=d(t,"H2",{class:!0,"data-svelte-h":!0}),h(e)!=="svelte-1j3dm6j"&&(e.textContent=l),i=f(t),s=d(t,"DIV",{class:!0,"data-svelte-h":!0}),h(s)!=="svelte-rarhtp"&&(s.innerHTML=c),this.h()},h(){v(e,"class","h2 mb-8 w-1/2 svelte-dp9awi"),v(s,"class","w-1/2")},m(t,a){o(t,e,a),o(t,i,a),o(t,s,a)},p:r,i:r,o:r,d(t){t&&(n(e),n(i),n(s))}}}class T extends w{constructor(e){super(),g(this,e,null,b,y,{})}}export{T as component};
