4a92123b1b40fb041852527de4776e782d9c5bb3
Marshall <marshall.bowles@gmail.com>
2023-12-15 14:24:41 -0800

    change layout of cards on xl screens

--- src/lib/CardItem.svelte
+++ src/lib/CardItem.svelte
@@ -11,7 +11,7 @@
 	export let icon: string = 'fa-circle';
 </script>
 
-<div class="card variant-outline m-4 flex w-3/4 flex-col items-center rounded-lg p-4 xl:w-1/3">
+<div class="card variant-outline m-4 flex w-3/4 flex-col items-center rounded-lg p-4 xl:hidden">
 	<a class="mb-8 w-2/3" href={url}>
 		<img src={image} alt={imageDescription} />
 	</a>
@@ -37,4 +37,36 @@
 			href={url}>More Info</a
 		>
 	{/if}
+</div>
+
+<div class="card variant-outline m-4 hidden w-3/4 flex-row justify-start rounded-lg p-4 xl:flex">
+	<a class="mb-8 mr-5 max-h-96 w-2/3" href={url}>
+		<img class="h-full" src={image} alt={imageDescription} />
+	</a>
+
+	<div class="flex flex-col">
+		<h3 class="h3 mb-4">
+			<span class="fa-solid {icon}"></span>
+			{title}
+		</h3>
+
+		<p class="mb-4">
+			{description}
+		</p>
+
+		<div class="flex flex-col items-center xl:items-start">
+			{#if purchaseLinks}
+				<PurchaseLinks items={purchaseLinks} />
+			{/if}
+		</div>
+
+		{#if url}
+			<a
+				class="card variant-ringed-secondary btn btn-sm card-hover mb-2 mt-4 h-1/3 max-h-10 w-1/4 self-end rounded-lg"
+				href={url}
+			>
+				More...
+			</a>
+		{/if}
+	</div>
 </div>
