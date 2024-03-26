493c85c0dc30ee26708473a86304aea3eadba530
Marshall Bowles <marshall.bowles@gmail.com>
2024-03-25 19:56:25 -0700

    change layout of cards for xl size screens

--- src/lib/PurchaseLinks.svelte
+++ src/lib/PurchaseLinks.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { PurchaseInfo } from "$lib/util/Purchase";
+	import type { PurchaseInfo } from '$lib/util/Purchase';
 
 	export let items: PurchaseInfo[];
 </script>
@@ -8,11 +8,10 @@
 	<h3 class="h3 mb-4">Get your copy now!</h3>
 {/if}
 
-<div class="flex flex-col items-center justify-evenly">
+<div class="flex flex-col items-center justify-evenly xl:flex-row xl:items-start xl:justify-start xl:content-start">
 	{#each items as link}
-		<a class="my-1 flex flex-col items-center"
-			href="{link.url}">
-			<img class="m-1 w-8/12" src={link.image} alt={link.label} />
+		<a class="my-1 flex flex-col items-center xl:items-start" href={link.url}>
+			<img class="mr-2 w-8/12 xl:w-11/12" src={link.image} alt={link.label} />
 		</a>
 	{/each}
 </div>
